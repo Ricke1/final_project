55aedc4513123eb38c9ff8ea4c081d4827f8b5f1
Ricke1 <dim.kyz.200@gmail.com>
2022-07-27 16:44:19 +0700

    Добавлен рабочий интерпретатор

--- work_start/test_items.py
+++ work_start/test_items.py
@@ -12,4 +12,3 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_add_to_basket(browser):
     browser.get(link)
     browser.find_element(By.CSS_SELECTOR, "#add_to_basket_form")
-
