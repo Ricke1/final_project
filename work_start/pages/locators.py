356a837abca805c3a00db8dcc9d7925066aba923
Ricke1 <dim.kyz.200@gmail.com>
2022-08-16 18:30:27 +0700

    Готовимся к новому этапу

--- work_start/pages/locators.py
+++ work_start/pages/locators.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 class MainPageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
+
 class LoginPageLocators:
     LOGIN_PAGE_URL = "https://selenium1py.pythonanywhere.com/accounts/login/"
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
